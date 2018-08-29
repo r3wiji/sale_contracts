f74ee183d8997c9c204f0991dde9ac6747f6d2d3
Valentin Peiro <valentin.peiro@wiji.io>
2018-08-29 14:04:52 +0200

    Update Wiji migration to deploy both contracts

--- migrations/2_deploy_wiji.js
+++ migrations/2_deploy_wiji.js
@@ -1,6 +1,15 @@
 var Wiji = artifacts.require("./wiji_token.sol");
+var Wiji_ICO = artifacts.require("./wiji_sale.sol");
+var wiji_instance;
 
 module.exports = function(deployer)
 {
-  deployer.deploy(Wiji);
+  deployer.deploy(Wiji).then(function()
+  {
+    return Wiji.deployed();
+  }).then(function(instance)
+  {
+    wiji_instance = instance;
+    return deployer.deploy(Wiji_ICO, Wiji.address);
+  });
 };
